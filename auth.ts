12fd881b93a057ab56833ae22f75b6a14774b2ee
sae-github <miyasae1126@gmail.com>
2024-03-10 13:52:11 +0900

    ✅: usersのselect処理をPrismaで書き換える対応

--- auth.ts
+++ auth.ts
@@ -2,14 +2,14 @@ import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
-import { sql } from '@vercel/postgres';
 import { User } from './app/lib/definitions';
 import bcrypt from 'bcrypt';
+import prisma from '@/app/lib/db';
 
 async function getUser(email: string): Promise<User | undefined> {
   try {
-    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-    return user.rows[0];
+    const user = await prisma.users.findMany({ where: { email: email } });
+    return user[0];
   } catch (error) {
     console.error('Failed to fetch user:', error);
     throw new Error('Failed to fetch user.');
